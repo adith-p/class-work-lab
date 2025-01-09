2590807973b3158fb239b94d2662d805ea4af3fe
adith-p <adithprakash008@gmail.com>
2025-01-09 23:21:19 +0530

    rest of the soln

--- 4b_copyfile.py
+++ 4b_copyfile.py
@@ -1,8 +1,14 @@
-with open("input1.txt") as file1:
-    with open("input2.txt","a") as file2:
+
+file1 = input("enter the file a")
+file2 = input("enter the file b")
+
+
+with open(file1) as file1:
+    with open(file2,"a") as file2:
         for line in file1:
             file2.write(line)
 
 
+print(" Copy pasting done ")
 
 
---
+++ 5a_string_op.py
@@ -0,0 +1,9 @@
+fname = input("enter the first name")
+lname = input("enter the last name")
+
+full_name = fname + lname
+
+middlename = full_name[2:6]
+
+full = " hello "
+single_full = ' kkk '
